a53783092fde774a1af49e7ea08e4496a9367f54
Andrew Mendez <andrew15722@yahoo.com>
2019-06-13 12:52:29 -0700

    finished refactoring the code in prototype-refactor.js for ES6 syntax

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -9,55 +9,102 @@ Prototype Refactor
 */
 
 //GameObject ===  //////////////////////
-const GameObject = function(obj) { 
-    // * createdAt
-    this.createdAt = obj.createdAt, 
-    // * name
-    this.name = obj.name,         
-    // * dimensions (These represent the character's size in the video game)
-    this.dimensions = obj.dimensions 
-  }
-  // * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-  GameObject.prototype.destroy = function() {  
+// const GameObject = function(obj) { 
+//     // * createdAt
+//     this.createdAt = obj.createdAt, 
+//     // * name
+//     this.name = obj.name,         
+//     // * dimensions (These represent the character's size in the video game)
+//     this.dimensions = obj.dimensions 
+//   }
+//   // * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+//   GameObject.prototype.destroy = function() {  
+//     return `${this.name} was removed from the game.`
+//   }
+
+//ES6 syntax
+class GameObject {
+    constructor(obj) {
+        this.createdAt = obj.createdAt,
+        this.name = obj.name,
+        this.dimensions = obj.dimensions 
+        this.healthPoints = obj.healthPoints
+    }
+
+    destroy() {
     return `${this.name} was removed from the game.`
-  }
-  
+    }
+}
   
   //=== CharacterStats////////////////////////////
-  const CharacterStats = function (obj) {
-    GameObject.call(this, obj);
-    //* healthPoints
-    this.healthPoints = obj.healthPoints
-  }  
-  //* should inherit destroy() from GameObject's prototype
-  CharacterStats.prototype = Object.create(GameObject.prototype)
-  //* takeDamage()
-  CharacterStats.prototype.takeDamage = function () {
-  //returns the string '<object name> took damage.'
-    return `${this.name} took damage.`;
-  }
-  
+//   const CharacterStats = function (obj) {
+//     GameObject.call(this, obj);
+//     //* healthPoints
+//     this.healthPoints = obj.healthPoints
+//   }  
+//   //* should inherit destroy() from GameObject's prototype
+//   CharacterStats.prototype = Object.create(GameObject.prototype)
+//   //* takeDamage()
+//   CharacterStats.prototype.takeDamage = function () {
+//   //returns the string '<object name> took damage.'
+//     return `${this.name} took damage.`;
+//   }
+
+// ES6 Syntax
+class CharacterStats extends GameObject {
+    constructor(obj) {
+        super(obj);
+    }
+
+    destroy() {
+        return `${this.name} was removed from the game.`
+    }
+
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+}
   
   
   // Humanoid (Having an appearance or character resembling that of a human.)
-  const Humanoid = function(obj) {
-    CharacterStats.call(this, obj);
-    //* team
-    this.team = obj.team,
-    //* weapons
-    this.weapons = obj.weapons,
-    //* language
-    this.language = obj.language
-  }
-  //* should inherit destroy() from GameObject through CharacterStats
-  //* should inherit takeDamage() from CharacterStats
-  Humanoid.prototype = Object.create(CharacterStats.prototype)
-  //* greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  Humanoid.prototype.greet = function () {
-    return `${this.name} offers a greeting in ${this.language}`
-  }
+//   const Humanoid = function(obj) {
+//     CharacterStats.call(this, obj);
+//     //* team
+//     this.team = obj.team,
+//     //* weapons
+//     this.weapons = obj.weapons,
+//     //* language
+//     this.language = obj.language
+//   }
+//   //* should inherit destroy() from GameObject through CharacterStats
+//   //* should inherit takeDamage() from CharacterStats
+//   Humanoid.prototype = Object.create(CharacterStats.prototype)
+//   //* greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+//   Humanoid.prototype.greet = function () {
+//     return `${this.name} offers a greeting in ${this.language}`
+//   }
   
-   
+// ES6 syntax
+class Humanoid extends GameObject {
+    constructor(obj) {
+        super(obj);
+        this.team = obj.team,
+        this.weapons = obj.weapons,
+        this.language = obj.language
+    }
+
+    destroy() {
+        return `${this.name} was removed from the game.`
+    }
+
+    takeDamage() {
+        return `${this.name} took damage.`;
+    }
+
+    greet () {
+        return `${this.name} offers a greeting in ${this.language}`
+    }
+}
   
   /*
     * Inheritance chain: GameObject -> CharacterStats -> Humanoid
